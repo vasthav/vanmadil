017b88ffbc5803cb8017692c15f27d11fe171383
kcsreevasthavan <sreevasthavan@gmail.com>
2014-07-10 22:06:36 +0530

    files deleted and created a new script file called new.sh

--- README.md
+++ README.md
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
-This is a small project for creating a firewall interface.
-It's mainly intended for a small network of computers.
-It's has many bash scripts that helps in configuring
-the netfilter iptables that comes preinstalled now in 
-all the new linux destros.
-
-Team:
-k c sreevasthavan (ME)
-arun john kurivilla
-kevin jose 
-giridhar nair
-=======
 This is a small project to create a firewall interface
 for a small local network. It basically uses the inbult
 netfilter iptables to configure the firewall which 
@@ -21,4 +8,4 @@ k c sreevasthavan (me)
 kevin jose
 arun john kuruvilla
 giridhar g nair
->>>>>>> d3fc394e839a672ff6dfa0327ab3802a784ff7e7
+
---
+++ new.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+WHITELIST=/usr/local/etc/whitelist.txt
+BLACKLIST=/usr/local/etc/blacklist.txt
+ALLOWED="22 25 53 80 443 465 587 993"
+IPTABLES=/sbin/iptables
+IPTABLES_SAVE=/sbin/iptables-save
+$IPTABLES_SAVE > /usr/local/etc/iptables.last
+$IPTABLES -P INPUT ACCEPT
+echo 'Setting default INPUT policy to ACCEPT'
+
+$IPTABLES -F
+echo 'Clearing Tables F'
+$IPTABLES -X
+echo 'Clearing Tables X'
+$IPTABLES -Z
+echo 'Clearing Tables Z'
+echo 'Allowing Localhost'
+$IPTABLES -A INPUT -s 127.0.0.1 -j ACCEPT
+
+## Whitelist
+for x in `grep -v ^# $WHITELIST | awk '{print $1}'`; do
+echo "Permitting $x..."
+$IPTABLES -A INPUT -s $x -i eth0 -p tcp -m multiport --dports 22,80,443 -j ACCEPT
+done
+
+for x in `grep -v ^# $WHITELIST | awk '{print $1}'`; do
+echo "Permitting $x..."
+$IPTABLES -A OUTPUT -d $x -o eth0 -p tcp -m multiport --dports 22,80,443 -j ACCEPT
+done
+
+## Blacklist
+#for x in `grep -v ^# $BLACKLIST | awk '{print $1}'`; do
+#echo "Denying $x..."
+#$IPTABLES -A INPUT -s $x -j DROP
+#done
+
+## Permitted Ports
+#for port in $ALLOWED; do
+#echo "Accepting port TCP $port..."
+#$IPTABLES -A INPUT -p tcp --dport $port -j ACCEPT
+#done
+
+#for port in $ALLOWED; do
+#echo "Accepting port UDP $port..."
+#$IPTABLES -A INPUT -p udp --dport $port -j ACCEPT
+#done
+
+$IPTABLES -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
+$IPTABLES -A INPUT -p udp -j DROP
+$IPTABLES -A INPUT -p tcp --syn -j DROP
+$IPTABLES -A OUTPUT -m state --state RELATED,ESTABLISHED,NEW -j ACCEPT
+iptables -A OUTPUT -j DROP
+iptables-save > /etc/iptables/rules.v4
